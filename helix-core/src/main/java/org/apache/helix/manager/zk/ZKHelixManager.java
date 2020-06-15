650add8f0b1502ceda61e8c052ff06e61c2ed5b0
Molly Gao <mgao@linkedin.com>
2020-06-15 09:48:46 -0700

    disable refresh

--- helix-core/src/main/java/org/apache/helix/manager/zk/ZKHelixManager.java
+++ helix-core/src/main/java/org/apache/helix/manager/zk/ZKHelixManager.java
@@ -173,7 +173,7 @@ public class ZKHelixManager implements HelixManager, IZkStateListener {
 
   /** Set this variable to negative can disable the message periodic refresh feature */
   private final long _messageRefreshTriggerInterval;
-  private static final long DEFAULT_PERIODIC_MESSAGE_REFRESH_INTERVAL = 5 * 60 * 1000;
+  private static final long DEFAULT_PERIODIC_MESSAGE_REFRESH_INTERVAL = -1;
 
   /**
    * status dump timer-task
